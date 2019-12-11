42e6f2f783738101500e8c2212026069cf364472
Jose <Jrivera6869@yahoo.com>
2019-12-11 18:43:29 -0500

    dealing with errors, got import to work

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
+import Apodinfo from "./Components/ApodInfo";
 
 function App() {
+  const [apod, setApod] = useState([]);
+  const [info, setInfo] = useState(``);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.nasa.gov/planetary/apod?api_key=WpPznJNV9aEIPZKlCfnAwvCDjhF8jNQuwzK8VX5e`
+      )
+      .then(response => {
+        const apodImg = response.data.hdurl;
+        console.log(response.data);
+        setApod(apodImg);
+      })
+      .catch(err => {
+        // console.log(err.message)
+      });
+  }, []);
+
   return (
     <div className="App">
+      <Apodinfo />
+      <img src={apod}></img>
+
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
+        EONET is the Earth Observatory Natural Event Tracker. EONET allows users
+        to browse the entire globe daily and look for natural events as they
+        occur. Regularly spotted storms in the tropics, dust storms over
+        deserts, forest fires in the summers. These events are constantly
+        occurring, and NASA NRT imagery can represent them all using a variety
+        of different data parameters.
       </p>
     </div>
   );
---
+++ src/Components/ApodInfo.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./App.css";
+
+function Apodinfo() {
+  return <h1>Astrononmy Picture of the Day </h1>;
+}
+
+export default Apodinfo;
