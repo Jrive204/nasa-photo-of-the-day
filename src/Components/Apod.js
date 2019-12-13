1a267b64a20e3a66d3649340eb0008026469daa0
Jose <Jrivera6869@yahoo.com>
2019-12-13 16:51:19 -0500

    done will change button

--- src/Components/Apod.js
+++ src/Components/Apod.js
@@ -6,34 +6,6 @@ function Apod(props) {
     if (!props.apod) return <h3>Loading...</h3>;
   }
 
-  // export const Image = styled.img`
-  //   width: 100%;
-  //   /* height: 80%; */
-  //   padding: 0%;
-  //   margin: 0%;
-  // `;
-
-  // export const Container = styled.div`
-  //   position: relative;
-  //   text-align: center;
-  //   color: white;
-  // `;
-
-  // export const Centerd = styled.div`
-  //   position: absolute;
-  //   top: 50%;
-  //   left: 50%;
-  //   transform: translate(-50%, -50%);
-  //   font-size: 1.2rem;
-  // `;
-
-  return (
-    <div>
-      {/* <img
-        className='backgroundimg'
-        style={{ height: `80vh` }}
-        src={props.apod}></img> */}
-    </div>
-  );
+  return <div></div>;
 }
 export default Apod;
